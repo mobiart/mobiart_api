5e743cd8fc9592e83ced7a5c32feed3358571de0
paulpou69 <paulicsde@gmail.com>
2020-11-21 00:55:40 +0200

    Add facebook redirect endpoint

--- auth/views.py
+++ auth/views.py
@@ -5,16 +5,34 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 
+import request
+
 
 from profile_manager.models import Profile
 from profile_manager.serializers import ProfileSerializer
 
-@api_view(["POST",])
+@api_view(["GET",])
 def index(request):
-    if request.method == "POST":
-        serializer = ProfileSerializer(data=request.data)
+    if request.method == "GET":
+        URL = "https://graph.facebook.com/v9.0/oauth/access_token?client_id=281582256624404&redirect_uri=https%3A%2F%2Fmilsugi.tech%2Fapi%2Fauth&oq=redirect_uri%3Dhttps%3A%2F%2Fmilsugi.tech%2Fapi%2Fauth&client_secret=6b0fce6ce450d741fcccbd2d8167b906&code="+request.GET["code"]
+        r = requests.get(url=URL)
+        token = r.json()["access_token"]
 
-        if serializer.is_valid():
-            serializer.save() 
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        r=requests.get(url="https://graph.facebook.com/me?access_token="+access_token)
+        uid = r.json()["id"]
+
+        try:
+            profile = Profile.objects.filter(user_id=id).get()
+            serializer = ProfileSerializer(profile)
+            return Response({"jwt":jwt.encode(serializer.data, settings.SECRET_KEY, algorithm='HS256')})
+
+
+        except Exception as e:
+            if str(e) == "Profile matching query does not exist.":
+                profile = Profile.objects.create(uid=request.POST["id_token"],access_token=name,email=user_data["email"],platform="apple")
+                profile.save()
+
+                serializer = ProfileSerializer(profile)
+                return Response({"jwt":jwt.encode(serializer.data, settings.SECRET_KEY, algorithm='HS256')})
+
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
