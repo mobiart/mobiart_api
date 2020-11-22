bac92437762f0b92260dcc4d145aecd204f5a23a
paulpou69 <paulicsde@gmail.com>
2020-11-22 09:13:32 +0200

    Solved serializer error

--- profile_manager/serializers.py
+++ profile_manager/serializers.py
@@ -1,6 +1,8 @@
 from django.contrib.auth.models import User, Group
 from rest_framework import serializers
 
+import requests
+
 from .models import Profile
 from marketplace.models import Bookmark 
 
