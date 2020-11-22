95bed42852b2156422e2042998362f98432cb50f
paulpou69 <paulicsde@gmail.com>
2020-11-22 11:07:52 +0200

    Added thumbnail field to product

--- marketplace/serializers.py
+++ marketplace/serializers.py
@@ -4,6 +4,17 @@ from rest_framework import serializers
 from .models import Product, Image
 
 class ProductSerializer(serializers.ModelSerializer):
+
+    thumbnail = serializers.SerializerMethodField('get_thumbnail')
+
+
+    def get_thumbnail(self, instance):
+        product_obj = Product.objects.filter(pk=instace.pk)
+        thumbnail = Image.objects.filter(product=product_obj).get()[0]
+
+        return thumbnail
+
+
     class Meta:
         model = Product
         fields = '__all__'
