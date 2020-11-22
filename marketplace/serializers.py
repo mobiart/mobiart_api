0743e0a1b6eeb4f64ef505cae977f89bd2e8080b
paulpou69 <paulicsde@gmail.com>
2020-11-22 09:03:15 +0200

    Solved ANOTHER error

--- marketplace/serializers.py
+++ marketplace/serializers.py
@@ -4,16 +4,9 @@ from rest_framework import serializers
 from .models import Product, Image
 
 class ProductSerializer(serializers.ModelSerializer):
-
-    pid = serializers.SerializerMethodField('get_pid')
-
-    def get_pid(self, instance):
-        pid = instance.pk
-        return pid
-
     class Meta:
         model = Product
-        fields = ["name","details","price","active"]
+        fields = '__all__'
 
 class ImageSerializer(serializers.ModelSerializer):
     class Meta:
